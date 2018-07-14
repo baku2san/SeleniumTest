fefcd10f2c8d72f20e443224b425dc94753d9ac2
shimaTaka <takanori23@hotmail.co.jp>
2018-07-14 22:39:42 +0900

    操作備忘録

--- SeleniumTest/SelenimuTest/MainWindow.xaml.cs
+++ SeleniumTest/SelenimuTest/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,12 +37,52 @@ namespace SelenimuTest
             // 「NoSuchElementException」が発生する場合があります。この場合は IE のインターネットオプションからセキュリティタブを選択し、全てのセキュリティゾーンに対して「保護モードを有効にする」へチェックしておきます。
         }
 
-        private void SearchGoogle(IWebDriver ie)
+        private void SearchGoogle(IWebDriver driver)
         {
-            ie.Url = "https://www.google.co.jp";
-            IWebElement element = ie.FindElement(By.Name("q"));
-            element.SendKeys("Cheese!");
+            driver.Url = "https://www.google.co.jp";
+            var element = driver.FindElement(By.Name("q"));
+            element.SendKeys("Cheese!");        // input text in the text
             element.Submit();
+            return;
+
+            // handle alerts
+            driver.SwitchTo().Alert().Accept();
+
+            driver.Navigate().GoToUrl("URL");
+            driver.Navigate().Back();
+            driver.Navigate().Forward();
+            driver.Navigate().Refresh();
+            driver.Close();
+            driver.Quit();
+            var currentUrl = driver.Url;
+            var pageTitle = driver.Title;
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.ClassName("classname")); //classでの指定
+            driver.FindElement(By.Id("id")); //idでの指定
+            driver.FindElement(By.XPath("xpath")); //xpathでの指定
+
+            var readText = element.Text;
+            var attribute = element.GetAttribute("value");
+            var isDisplayed = element.Displayed;
+            var isEnalbed = element.Enabled;
+            var isSelected = element.Selected;
+            element.Click();
+
+            // scroll to the element
+            var actions = new Actions(driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+
+            // select dropdown 
+            var selectElement = new SelectElement(element);
+            selectElement.SelectByIndex(5); // indexで選択
+            selectElement.SelectByValue("value"); // valueの値
+            selectElement.SelectByText("text"); // 表示テキスト
+
+            // Selenium API
+            //            http://www.seleniumqref.com/api/webdriver_gyaku.html
+
         }
     }
 }
